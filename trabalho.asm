050bf49cbbf7669ef81abad4205b612ca717078f
gondimjoaom <gondimjoaom@gmail.com>
2018-11-15 17:09:58 -0300

    logica feita

--- trabalho.asm
+++ trabalho.asm
@@ -1,17 +1,21 @@
 %include "asm_io.inc"
 segment .data
 
+no0 db		"No 0", 0
+
 varM	db 0
 varN	db 0
 varS	db 0
 varT	db 0
 varB	db 0
 final	db 0
-buracos		db 0
+var		db 0
+;buracos		db 0
+buracos	times 300 db 51
 
 segment .bss
 
-vectPath	resw 300
+vect	resb 300
 
 segment .text
 		global asm_main
@@ -20,6 +24,8 @@ asm_main:
 
 	enter 0,0
 	pusha
+	mov		eax, 0
+	mov		[buracos + 0], eax
 	;read N
 	call	read_int
 	mov		[varN], eax
@@ -29,7 +35,7 @@ asm_main:
 	call	read_int
 	mov		[varM], eax
 	
-	mov		ecx, [varM]
+	mov		ecx, eax
 	mov		ebx, 0
 	
 	readVector:
@@ -37,16 +43,77 @@ asm_main:
 		call	read_int
 		mov		[vect + ebx], eax
 		;add T
-		add		ebx, 2
+		add		ebx, 1
 		call	read_int
 		mov		[vect + ebx], eax
 		;add B
-		add		ebx, 2
+		add		ebx, 1
 		call	read_int
 		mov		[vect + ebx], eax
-		loop	readVector
 		
+		loop	readVector
+	
+	add		ebx, 1
+	mov		eax, 55
+	mov		[vect + ebx], eax
+	
 	mov		ebx, 0
+	
+	call func_vet	
+	
+;algoritmo para coletar menores qtds de buracos	
+func_vet:
+	enter	12, 0
+	
+	mov		eax, [vect + ebx] ;fonte
+	mov		[ebp-4], eax ;liberando eax
+	
+	add		ebx, 1
+	mov		ecx, [vect + ebx] ;destino
+	mov		[ebp-8], ecx ;liberando ecx
+	
+	add		ebx, 1
+	mov		edx, [vect + ebx] ;qtd de buracos
+	mov		[ebp-12], edx; liberando edx	
+	
+	
+	
+	
+	mov		eax, [ebp-4]
+	call	print_int
+	;call	print_nl
+	
+	;debug
+	;	mov 	eax, no0
+	;	call	print_string
+	;	call	print_nl
+	
+	mov		edx, [buracos + eax] ;armezenando qtd de buracos na fonte		
+	add		edx, [ebp-12] ;somando qtd de buracos para destino
+	
+	cmp		[buracos + ecx], edx ;comparando c/ qtd de buracos existente
+	jg		bigger
+	mov		[buracos + ecx], edx ;se for menor, colocar a qtd de buracos nova
+	bigger:
+	
+	add ebx, 1
+	mov		eax, 55
+	cmp		[buracos + ebx], eax
+	jz		finaliza
+	call	func_vet
+	finaliza:
+	leave
+	ret
+	
+sub		ebx, 1
+mov		eax,[buracos + ebx]	
+call 	print_int
+
+call	print_nl
+        popa
+        mov     eax, 0
+        leave                     
+        ret
 		
 	
 		
